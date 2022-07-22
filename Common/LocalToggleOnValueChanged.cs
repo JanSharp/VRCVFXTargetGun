856e7ffa9810a6a7be52a48880966b599e2d4bf3
JanSharp <steinmeister36@gmail.com>
2022-07-22 21:10:41 +0200

    Add tabbed pane in help/info window

---
+++ Common/LocalToggleOnValueChanged.cs
@@ -0,0 +1,23 @@
+﻿using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace JanSharp
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class LocalToggleOnValueChanged : UdonSharpBehaviour
+    {
+        public Toggle toggle;
+        [Tooltip("By toggle it really means setting the active state equal to isOn state of the toggle.")]
+        public GameObject[] objectsToToggle;
+
+        public void OnValueChanged()
+        {
+            if (toggle != null && objectsToToggle != null)
+                foreach (GameObject obj in objectsToToggle)
+                    obj.SetActive(toggle.isOn);
+        }
+    }
+}
