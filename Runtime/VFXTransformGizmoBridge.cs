e01e70e981f9e4ddd28fe18820948c296e2f02fb
JanSharp <steinmeister36@gmail.com>
2026-09-07 00:28:51 +0200

    Update due to new gizmo bridge abstract method

--- Runtime/VFXTransformGizmoBridge.cs
+++ Runtime/VFXTransformGizmoBridge.cs
@@ -1,7 +1,6 @@
 ﻿using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
-using VRC.Udon;
 
 namespace JanSharp
 {
@@ -60,6 +59,11 @@ namespace JanSharp
             if (!isInVR)
                 return Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl);
             return false;
+        }
+
+        public override bool ShowVisualRaycastThisFrame()
+        {
+            return isInVR;
         }
 
         public override void OnPositionModified()
